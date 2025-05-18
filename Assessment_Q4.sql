2c0ced17846a0b44ab75d4bd9ccf6bf6b200fa49
Muhammad Abdulmateen <107678560+Adeizah@users.noreply.github.com>
2025-05-18 16:29:04 +0100

    Update Assessment_Q4.sql

--- Assessment_Q4.sql
+++ Assessment_Q4.sql
@@ -2,10 +2,10 @@
 
 SELECT 
 	owner_id, 
-    concat(first_name, " ", last_name) as name, 
-    timestampdiff(Month, date_joined, current_date()) as tenure_months,
-    round(sum(confirmed_amount),2) as total_transactions,
-    round(sum(confirmed_amount) / timestampdiff(Month, date_joined, current_date()) * 12 * 0.1, 2) as CLV
+	concat(first_name, " ", last_name) as name, 
+	timestampdiff(Month, date_joined, current_date()) as tenure_months,
+	round(sum(confirmed_amount),2) as total_transactions,
+	round(sum(confirmed_amount) / timestampdiff(Month, date_joined, current_date()) * 12 * 0.1, 2) as CLV
 FROM users_customuser as users
 LEFT JOIN savings_savingsaccount as savings
 	ON users.id = savings.owner_id
